2172802a5bfbb39add784107afbb3478c1d77cff
Chris <51767118+AlgNinja@users.noreply.github.com>
2025-05-01 02:18:21 -0400

    Create mergesort.js

---
+++ mergesort.js
@@ -0,0 +1,2 @@
+m=(o,t,a=b=0,n=[])=>{for(;a<o.length&&b<t.length;n.push(o[a]<t[b]?o[a++]:t[b++]));return n.concat(o.slice(a),t.slice(b))};s=(o,c=o.length>>1)=>o.length<2?o:m(s(o.slice(0, c)),s(o.slice(c)))
+//189 characters
